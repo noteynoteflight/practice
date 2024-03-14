7cc8b760fee4533428431e10acde3d7887444650
noteynoteflight <goncharovgleb7@gmail.com>
2024-03-14 15:59:34 +0200

    m

--- Groups/Implementions/PersonEntity.cs
+++ Groups/Implementions/PersonEntity.cs
@@ -2,5 +2,6 @@ namespace Groups.Implementions;
 
 public class PersonEntity
 {
-
+    public string Name { get; set; }
+    public int Age { get; set; }
 }
---
+++ Groups/Tests/PersonEntityTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Groups.Tests
+{
+    [TestFixture]
+    public class PersonEntityTests
+    {
+        [Test]
+        public void Person_ConstructsCorrectly()
+        {
+            var person = new Person();
+            Assert.IsNotNull(person);
+            Assert.IsNull(person.Name);
+            Assert.AreEqual(0, person.Age);
+        }
+
+
+    }
+}
