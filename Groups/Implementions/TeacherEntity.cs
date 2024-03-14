fa75a16cd79ea341a7bc9027af316bae062e9193
noteynoteflight <goncharovgleb7@gmail.com>
2024-03-14 16:10:09 +0200

    step6

--- Groups/Implementions/TeacherEntity.cs
+++ Groups/Implementions/TeacherEntity.cs
@@ -4,5 +4,11 @@ namespace Students.Implementations;
 
 public class TeacherEntity : PersonEntity
 {
+    public int TeacherId { get; set; }
+    public List<SubjectEntity> Subjects { get; set; }
 
+    public TeacherEntity()
+    {
+        Subjects = new List<SubjectEntity>();
+    }
 }
---
+++ Groups/Tests/TeacherEntityTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Groups.Tests
+{
+    [TestFixture]
+    public class TeacherEntityTests
+    {
+        [Test]
+        public void TeacherEntity_ConstructsCorrectly()
+        {
+            var teacher = new TeacherEntity();
+            Assert.IsNotNull(teacher);
+            Assert.IsNull(teacher.Name);
+            Assert.AreEqual(0, teacher.Age);
+            Assert.AreEqual(0, teacher.TeacherId);
+            Assert.IsNotNull(teacher.Subjects);
+            Assert.AreEqual(0, teacher.Subjects.Count);
+        }
+    }
+}
