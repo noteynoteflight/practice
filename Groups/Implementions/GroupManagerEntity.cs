5fa8ce48665c761189f4b62faf772302a0ce77e2
noteynoteflight <goncharovgleb7@gmail.com>
2024-03-14 15:52:06 +0200

    step2

--- Groups/Implementions/GroupManagerEntity.cs
+++ Groups/Implementions/GroupManagerEntity.cs
@@ -4,5 +4,20 @@ namespace Groups.Implementions;
 
 public class GroupManagerEntity
 {
+    public List<GroupEntity> Groups { get; set; }
 
+    public GroupManagerEntity()
+    {
+        Groups = new List<GroupEntity>();
+    }
+
+    public void AddGroup(GroupEntity group)
+    {
+        Groups.Add(group);
+    }
+
+    public void RemoveGroup(GroupEntity group)
+    {
+        Groups.Remove(group);
+    }
 }
---
+++ Groups/Tests/GroupEntityManagerTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Groups.Tests
+{
+    [TestFixture]
+    public class GroupManagerEntityTests
+    {
+        [Test]
+        public void GroupManagerEntity_ConstructsCorrectly()
+        {
+            var groupManager = new GroupManagerEntity();
+
+            Assert.IsNotNull(groupManager);
+            Assert.IsNotNull(groupManager.Groups);
+            Assert.AreEqual(0, groupManager.Groups.Count);
+        }
+
+        [Test]
+        public void GroupManagerEntity_AddGroup_AddsCorrectly()
+        {
+            var groupManager = new GroupManagerEntity();
+            var group = new GroupEntity();
+
+            groupManager.AddGroup(group);
+
+            Assert.AreEqual(1, groupManager.Groups.Count);
+            Assert.IsTrue(groupManager.Groups.Contains(group));
+        }
+
+    }
+}
