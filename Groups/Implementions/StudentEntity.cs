74fa688092787c56756aa69fb6416837e9355f6e
noteynoteflight <goncharovgleb7@gmail.com>
2024-03-14 16:02:20 +0200

    step4

--- Groups/Implementions/StudentEntity.cs
+++ Groups/Implementions/StudentEntity.cs
@@ -2,5 +2,11 @@ namespace Groups.Implementions;
 
 public class StudentEntity : PersonEntity
 {
+    public int StudentId { get; set; }
+    public List<SubjectEntity> Subjects { get; set; }
 
+    public StudentEntity()
+    {
+        Subjects = new List<SubjectEntity>();
+    }
 }
---
+++ Groups/Tests/StudentEntityTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Groups.Tests
+{
+    public class StudentEntityTests
+    {
+        [Test]
+        public void StudentEntity_ConstructsCorrectly()
+        {
+            var student = new StudentEntity();
+            Assert.IsNotNull(student);
+            Assert.IsNull(student.Name);
+            Assert.AreEqual(0, student.Age);
+            Assert.AreEqual(0, student.StudentId);
+            Assert.IsNotNull(student.Subjects);
+            Assert.AreEqual(0, student.Subjects.Count);
+        }
+
+        [Test]
+        public void StudentEntity_StudentId_SetCorrectly()
+        {
+            var student = new StudentEntity();
+            var studentId = 123;
+            student.StudentId = studentId;
+            Assert.AreEqual(studentId, student.StudentId);
+        }
+    }
