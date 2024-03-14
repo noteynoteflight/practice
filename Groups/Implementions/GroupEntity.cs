94eef3a5d204c7390490c0bc9c4356e3fff6d914
noteynoteflight <goncharovgleb7@gmail.com>
2024-03-14 15:38:49 +0200

    GroupEntity

--- Groups/Implementions/GroupEntity.cs
+++ Groups/Implementions/GroupEntity.cs
@@ -4,5 +4,7 @@ namespace Groups.Implementions;
 
 public class GroupEntity
 {
-
+    public string Name { get; set; }
+    public List<StudentEntity> Students { get; set; }
+    public TeacherEntity Teacher { get; set; }
 }
--- Groups/Tests/GroupEntityTests.cs
+++ Groups/Tests/GroupEntityTests.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace Groups.Tests
 {
-    class GroupEntityTests
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class GroupEntityTests
     {
+        [Test]
+        public void GroupEntity_ConstructsCorrectly()
+        {
+            var group = new GroupEntity();
+            Assert.IsNotNull(group);
+            Assert.IsNull(group.Name);
+            Assert.IsNotNull(group.Students);
+            Assert.AreEqual(0, group.Students.Count);
+            Assert.IsNull(group.Teacher);
+        }
+
+        [Test]
+        public void GroupEntity_Name_SetCorrectly()
+        {
+            var group = new GroupEntity();
+            var groupName = "GroupA";
+            group.Name = groupName;
+            Assert.AreEqual(groupName, group.Name);
+        }
+
     }
 }
